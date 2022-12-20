18ba6e495b29392e580f415317a0e9c7a41dc33c
Pade88 <ovidiu.padeanu@gmail.com>
2022-12-20 20:26:47 +0200

    Initial Commit

---
+++ TextCompare/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TextCompare
+//
+//  Created by Ovidiu Padeanu on 20.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextCompare/TextCompareApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextCompareApp.swift
+//  TextCompare
+//
+//  Created by Ovidiu Padeanu on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TextCompareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
